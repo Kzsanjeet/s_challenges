f28e08a986c26c54bc689a2c0975d929d1a981de
Kzsanjeet <127291186+Kzsanjeet@users.noreply.github.com>
2023-08-02 20:39:41 +0545

    updated RPS game

--- RPS/script.js
+++ RPS/script.js
@@ -1,58 +1,70 @@
-
 const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
 const scissor = document.getElementById("scissor")
 const display = document.getElementById("dis")
+const playerWin = document.getElementById("playerwin")
+const computerWin = document.getElementById("computerwin")
+const drawMatch = document.getElementById("draw")
+
+let total = 0
+let  comp = 0
+let draw = 0
 
 let computerChoice = ()=>{
 
     let choice = ["rock","paper",'scissor']
     let computer = choice[Math.floor(Math.random()*choice.length)]
-    // console.log(computer)
     return computer
-
 }
-
-
-
-function playGame(user,computer){
+function playGame(user,computer){   
     if(user === computer){
         let result = "It is tie"
+        if (result === "It is tie"){
+            let e = draw += 1
+            drawMatch.textContent = e
+            console.log(draw)
+        }
+        console.log(result)
         display.textContent = result
     }else if(user === "rock" && computer === "scissor"){
         let result = "You win"
+        if (result === "You win"){
+            let d = total+=1
+            playerWin.textContent = d
+        }
+        console.log(total)
         display.textContent= result
-        //return "You win"
         console.log('You win')
     }else if(user ==="paper"&& computer ==="rock"){
         let result = "You win"
+        if (result === "You win"){
+            let c = total+=1
+            playerWin.textContent = "Player"
+            playerWin.textContent = c
+        }
         display.textContent= result
-        // return "You win"
-        console.log('You win')
     }else if (user === "scissor" && computer === "paper"){
         let result = "You win"
+        if (result === "You win"){
+            let b = total+=1
+            playerWin.textContent = b
+        }
         display.textContent= result
-        // return "You win!"
-        console.log('You win')
     }else{
         let result = "You loose"
+        if( result === "You loose"){
+            let a = comp+=1
+            computerWin.textContent = a
+        }
         display.textContent = result
-        // return "You defeated.."
-        console.log('You loose')
     }
-    
-
 }
-// function playGame(v,n){
-//     console.log("hello")
-// }
-
 rock.addEventListener("click",()=>{
-    // console.log(computerChoice())
     playGame('rock',computerChoice())
 })
 paper.addEventListener('click',()=>{
     playGame('paper',computerChoice())
+    
 })
 scissor.addEventListener("click",()=>{
     playGame('scissor',computerChoice())
