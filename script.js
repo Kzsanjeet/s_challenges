dd853b73678ffbf3f5ceec8e695629cbcf423f7e
Kzsanjeet <127291186+Kzsanjeet@users.noreply.github.com>
2023-08-16 23:13:19 +0545

    added temperature converter

--- script.js
+++ script.js
@@ -1,60 +1,22 @@
-const display = document.querySelector(".display")
-const btn = document.getElementById("btn")
+const input = document.getElementById("inputs")
+const btn1 = document.getElementById("btn1")
+const btn2 = document.getElementById("btn2")
+const result = document. getElementById("output")
 
-
-let change = ()=>{
-    const inputs = document.getElementById("input")
-    let sum = 0
-    inputs+= sum
-    display.textContent = inputs.value
+let celcius = () =>{
+    let c =  (input.value- 32) * 5/9;
+    result.textContent = c
+    Math.floor(c)
+}
+let farenheit = () =>{
+    let f= (input.value * 9/5) + 32
+    result.textContent = f
+    Math.floor(f)
 }
 
-btn.addEventListener("click",()=>{
-    change()
+btn1.addEventListener("click",()=>{
+    celcius()
 })
-
-
-
-// const btn = document.getElementById('btn')
-// const disp = document.querySelector(".result")
-
-// const listy = []
-// const store = () =>{
-//     const vals = document.getElementById('inputv')
-//     listy.push(vals.value)
-//     let output = listy.join(", ")
-//     disp.textContent = output
-//     console.log(listy)
-// }
-
-// btn.addEventListener('click',()=>{
-//     store()
-//     inputV.value = ''
-
-
-
-// const btn = document.getElementById('btn')
-// const disp = document.querySelector(".result")
-
-// const listy = []
-// const store = () =>{
-//     const vals = document.getElementById('inputv')
-//     listy.push(vals.value)
-//     // let output = listy.join(", ")
-
-//     //creating unordred list
-//     let ulEle = document.createElement("ol")
-//     for(i=0;i<listy.length;i++){
-//         let lstEle = document.createElement("li")
-//         lstEle.textContent = listy[i]
-//         ulEle.appendChild(lstEle)
-//     }
-//     disp.innerHTML=''
-//     disp.appendChild(ulEle)
-    
-//     vals.value = ''
-// }
-
-// btn.addEventListener('click',()=>{
-//     store()
-// })
+btn2.addEventListener("click",()=>{
+    farenheit()
+})
