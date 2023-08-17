4f5c208876f17fdf53f398db24c60f76d8cb2ae7
Kzsanjeet <127291186+Kzsanjeet@users.noreply.github.com>
2023-08-17 19:41:58 +0545

    basic to-to list practice

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-const input = document.getElementById("inputs")
-const btn1 = document.getElementById("btn1")
-const btn2 = document.getElementById("btn2")
-const result = document. getElementById("output")
-
-let celcius = () =>{
-    let c =  (input.value- 32) * 5/9;
-    result.textContent = c
-    Math.floor(c)
+let infor = document.getElementById("info")
+let btn1 = document.getElementById("btn1")
+let list1 = []
+function addHere(){
+    let inputValue = document.getElementById("inputs")
+    list1.push(inputValue.value)
+    infor.textContent = list1 
 }
-let farenheit = () =>{
-    let f= (input.value * 9/5) + 32
-    result.textContent = f
-    Math.floor(f)
-}
-
 btn1.addEventListener("click",()=>{
-    celcius()
-})
-btn2.addEventListener("click",()=>{
-    farenheit()
+    addHere()
 })
