8058c37be8baf8a6b65d42a388ee208463b712a9
Kzsanjeet <127291186+Kzsanjeet@users.noreply.github.com>
2023-09-23 00:32:07 +0545

    updated js and html for blog

--- blog/script.js
+++ blog/script.js
@@ -1,3 +1,5 @@
+let writeBtn = document.querySelector("#writeDown")
+let showBtn = document.getElementById("show")
 let saveBtn = document.getElementById("save")
 let deleteBtn = document.getElementById("delete")
 let savePara = document.querySelector(".savePara")
@@ -5,8 +7,42 @@ let content = document.getElementById("content")
 let deleteNumber = document.getElementById("blogDel")
 let blogNumber = document.getElementById("blogNumber")
 let delPara = document.querySelector(".delPara")
-let showBtn = document.getElementById("show")
 let showParag = document.getElementById("showPara")
+let saveSec = document.querySelector(".saveSec")
+let history = document.getElementById("History")
+let returnBtn = document.getElementById("return")
+let returnHisBtn = document.getElementById("returnHis")
+const historyDatas = document.querySelector(".history-datas")
+
+
+returnBtn.addEventListener("click",()=>{
+    writeBtn.style.display= "flex"
+    history.style.display = "none"
+    showBtn.style.display = "flex"
+    saveSec.style.display = "none"
+})
+returnHisBtn.addEventListener("click",()=>{
+    writeBtn.style.display= "flex"
+    history.style.display = "none"
+    showBtn.style.display = "flex"
+    saveSec.style.display = "none"
+})
+
+writeBtn.addEventListener("click",()=>{
+    saveSec.style.display = "flex"
+    history.style.display = "none"
+    writeBtn.style.display = "none"
+    showBtn.style.display = "none"
+    console.log("clicked")
+})
+showBtn.addEventListener("click",()=>{
+    history.style.display = "flex"
+    showBtn.style.display = "none"
+    saveSec.style.display = "none"
+    showBtn.style.display = "none"
+    writeBtn.style.display = "none"
+    console.log("click")
+})
 
 function saveData(){
         localStorage.setItem(blogNumber.value,content.value)
@@ -26,9 +62,22 @@ function deleteData(){
     }
 }
 function showData(){
-    showParag.innerHTML = JSON.stringify(localStorage)
-}
+    // let a = JSON.stringify(localStorage)
+    let keys = Object.keys(localStorage)
+     console.log(keys)
+    for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    
+    var data = localStorage.getItem(key);
+    
+    console.log(data)
+    // historyDatas.innerHTML = `
+    // <div id='title'>${a.}`
 
+    // showParag.innerHTML = `<h2>All the restored blogs:<h2><br>${JSON.stringify(localStorage)}`
+    // console.log(localStorage)
+}
+}
 deleteBtn.addEventListener("click",()=>{
     deleteData()   
     deleteNumber.value = "" 
