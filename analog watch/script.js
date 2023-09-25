590a63eebe297e3ecdb187d1ee33fe50a29974d8
Kzsanjeet <127291186+Kzsanjeet@users.noreply.github.com>
2023-09-25 23:32:42 +0545

    proper completion

--- analog watch/script.js
+++ analog watch/script.js
@@ -10,6 +10,6 @@ setInterval(()=>{
     srotation = 6*stime
 
     hour.style.transform = `rotate(${hrotation}deg)`
-    minute.style.transform = `rotate(${hrotation}deg)`
-    second.style.transform = `rotate(${hrotation}deg)`
+    minute.style.transform = `rotate(${mrotation}deg)`
+    second.style.transform = `rotate(${srotation}deg)`
 },2000)
