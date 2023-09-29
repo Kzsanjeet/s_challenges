a7b0642d4a5b63384cf3f731870f588dec258f20
Kzsanjeet <127291186+Kzsanjeet@users.noreply.github.com>
2023-09-29 18:08:54 +0545

    updated and proper completed for the first page

--- simple e-commerce/script.js
+++ simple e-commerce/script.js
@@ -1,8 +1,15 @@
 let card1 = document.querySelector(".card1")
+let card2 = document.querySelector(".card2")
+let card3 = document.querySelector(".card3")
+let card4 = document.querySelector(".card4")
 
 let slideIndex = 0;
 const slideShow = ()=>{
     card1.style.transform = `translateX(-${slideIndex*5}%)`
+    card2.style.transform = `translateX(-${slideIndex*5}%)`
+    card3.style.transform = `translateX(-${slideIndex*5}%)`
+    card4.style.transform = `translateX(-${slideIndex*5}%)`
+    
 }
 let showAllProduct = ()=>{
     let f = fetch("product.json")
@@ -11,8 +18,15 @@ let showAllProduct = ()=>{
     }).then((result)=>{
     console.log(result)
     let shoesData = result.shoes
+    let tshirtsData= result.tshirts
+    let pantsData = result.pants
+    console.log(pantsData)
+    console.log(tshirtsData)
     console.log(shoesData)
     cardOne(result)
+    cardTwo(result)
+    cardThree(result)
+    cardfour(result)
 })
 }
 showAllProduct()
@@ -23,7 +37,8 @@ let cardOne = (result)=>{
     for(let i= 0; i<shoesData.length; i++){
         card1.innerHTML+=`
         <div class= "card" data-card-id='${shoesData[i].id}'>
-        <div class= "imageBox">${shoesData[i].image}</div>
+        <div class= "imageBox">
+        <img src = "${shoesData[i].image}" alt="image"</div>
         <p  class = 'shoeName'>Price: ${shoesData[i].name}</P>
         <p  class = 'shoeBrand'>Brand: ${shoesData[i].price}</P>
         <p class = 'shoeRating'>Ratings: ${shoesData[i].ratings}</p>
@@ -32,8 +47,61 @@ let cardOne = (result)=>{
     }
 
 }
+
+let cardTwo = (result)=>{
+    let tshirtsData = result.tshirts
+    card2.innerHTML=""
+    for(let i= 0; i<tshirtsData.length; i++){
+        card2.innerHTML+=`
+        <div class= "card" data-card-id='${tshirtsData[i].id}'>
+        <div class= "imageBox">
+        <img src = "${tshirtsData[i].image}" alt="image"</div>
+        <p  class = 'shoeName'>Price: ${tshirtsData[i].name}</P>
+        <p  class = 'shoeBrand'>Brand: ${tshirtsData[i].price}</P>
+        <p class = 'shoeRating'>Ratings: ${tshirtsData[i].ratings}</p>
+        </div>
+        `
+    }
+
+}
+
+let cardThree = (result)=>{
+    let pantsData = result.pants
+    card3.innerHTML=""
+    for(let i= 0; i<pantsData.length; i++){
+        card3.innerHTML+=`
+        <div class= "card" data-card-id='${pantsData[i].id}'>
+        <div class= "imageBox">
+        <img src = "${pantsData[i].image}" alt="image"</div>
+        <p  class = 'shoeName'>Price: ${pantsData[i].name}</P>
+        <p  class = 'shoeBrand'>Brand: ${pantsData[i].price}</P>
+        <p class = 'shoeRating'>Ratings: ${pantsData[i].ratings}</p>
+        </div>
+        `
+    }
+}
+
+let cardfour = (result)=>{
+    let jacketsData = result.jackets
+    card4.innerHTML=""
+    for(let i= 0; i<jacketsData.length; i++){
+        card4.innerHTML+=`
+        <div class= "card" data-card-id='${jacketsData[i].id}'>
+        <div class= "imageBox">
+        <img src = "${jacketsData[i].image}" alt="image"</div>
+        <p  class = 'shoeName'>Price: ${jacketsData[i].name}</P>
+        <p  class = 'shoeBrand'>Brand: ${jacketsData[i].price}</P>
+        <p class = 'shoeRating'>Ratings: ${jacketsData[i].ratings}</p>
+        </div>
+        `
+    }
+}
+
+
+
+
 function selfServ(){
-    if(slideIndex < card1.childElementCount -1){
+    if(slideIndex < card1.childElementCount -1 || card2.childElementCount -1|| card3.childElementCount -1){
         slideIndex++;
     }else{
         slideIndex = 0;
