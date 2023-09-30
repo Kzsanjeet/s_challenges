1a25cd799ace3ce51123ab4f1feb03197c475a39
Kzsanjeet <127291186+Kzsanjeet@users.noreply.github.com>
2023-09-30 23:09:45 +0545

    corrected error in price and brand

--- simple e-commerce/script.js
+++ simple e-commerce/script.js
@@ -2,13 +2,15 @@ let card1 = document.querySelector(".card1")
 let card2 = document.querySelector(".card2")
 let card3 = document.querySelector(".card3")
 let card4 = document.querySelector(".card4")
+let nextButton = document.getElementById("next")
+let prevButton = document.getElementById("prev")
 
 let slideIndex = 0;
 const slideShow = ()=>{
     card1.style.transform = `translateX(-${slideIndex*5}%)`
-    card2.style.transform = `translateX(-${slideIndex*5}%)`
+    card2.style.transform = `translateX(-${slideIndex*10}%)`
     card3.style.transform = `translateX(-${slideIndex*5}%)`
-    card4.style.transform = `translateX(-${slideIndex*5}%)`
+    card4.style.transform = `translateX(-${slideIndex*15}%)`
     
 }
 let showAllProduct = ()=>{
@@ -39,8 +41,8 @@ let cardOne = (result)=>{
         <div class= "card" data-card-id='${shoesData[i].id}'>
         <div class= "imageBox">
         <img src = "${shoesData[i].image}" alt="image"</div>
-        <p  class = 'shoeName'>Price: ${shoesData[i].name}</P>
-        <p  class = 'shoeBrand'>Brand: ${shoesData[i].price}</P>
+        <p  class = 'shoeName'>Price: $ ${shoesData[i].price}</P>
+        <p  class = 'shoeBrand'>Brand: ${shoesData[i].brand}</P>
         <p class = 'shoeRating'>Ratings: ${shoesData[i].ratings}</p>
         </div>
         `
@@ -56,8 +58,8 @@ let cardTwo = (result)=>{
         <div class= "card" data-card-id='${tshirtsData[i].id}'>
         <div class= "imageBox">
         <img src = "${tshirtsData[i].image}" alt="image"</div>
-        <p  class = 'shoeName'>Price: ${tshirtsData[i].name}</P>
-        <p  class = 'shoeBrand'>Brand: ${tshirtsData[i].price}</P>
+        <p  class = 'shoeName'>Price: $ ${tshirtsData[i].price}</P>
+        <p  class = 'shoeBrand'>Brand: ${tshirtsData[i].brand}</P>
         <p class = 'shoeRating'>Ratings: ${tshirtsData[i].ratings}</p>
         </div>
         `
@@ -73,8 +75,8 @@ let cardThree = (result)=>{
         <div class= "card" data-card-id='${pantsData[i].id}'>
         <div class= "imageBox">
         <img src = "${pantsData[i].image}" alt="image"</div>
-        <p  class = 'shoeName'>Price: ${pantsData[i].name}</P>
-        <p  class = 'shoeBrand'>Brand: ${pantsData[i].price}</P>
+        <p  class = 'shoeName'>Price: $ ${pantsData[i].price}</P>
+        <p  class = 'shoeBrand'>Brand: ${pantsData[i].brand}</P>
         <p class = 'shoeRating'>Ratings: ${pantsData[i].ratings}</p>
         </div>
         `
@@ -89,19 +91,22 @@ let cardfour = (result)=>{
         <div class= "card" data-card-id='${jacketsData[i].id}'>
         <div class= "imageBox">
         <img src = "${jacketsData[i].image}" alt="image"</div>
-        <p  class = 'shoeName'>Price: ${jacketsData[i].name}</P>
-        <p  class = 'shoeBrand'>Brand: ${jacketsData[i].price}</P>
+        <p  class = 'shoeName'>Price: $ ${jacketsData[i].price}</P>
+        <p  class = 'shoeBrand'>Brand: ${jacketsData[i].brand}</P>
         <p class = 'shoeRating'>Ratings: ${jacketsData[i].ratings}</p>
         </div>
         `
     }
 }
 
+nextButton.addEventListener("click"),()=>{
 
+    selfServ()
+}
 
 
 function selfServ(){
-    if(slideIndex < card1.childElementCount -1 || card2.childElementCount -1|| card3.childElementCount -1){
+    if(slideIndex < card1.childElementCount -1){
         slideIndex++;
     }else{
         slideIndex = 0;
