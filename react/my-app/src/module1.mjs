aee245ff19a9eb7df1ddd9f4c53b0c3c320d0b7d
Kzsanjeet <sanjeetkazithapa@gmail.com>
2024-06-05 00:13:32 +0545

    learned about props in react

---
+++ react/my-app/src/module1.mjs
@@ -0,0 +1,6 @@
+const s = "Sanjeet";
+const b = "Balbir";
+const d = "deepak";
+const e = "ejen";
+
+export { s, b, d, e }; // Named exports
---
+++ react/my-app/src/module2.mjs
@@ -0,0 +1,6 @@
+import { s, b, d, e } from "./module1.mjs";
+
+console.log(s);
+console.log(b);
+console.log(d);
+console.log(e);
