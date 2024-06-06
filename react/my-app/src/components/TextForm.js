39ad799e61a7b8594c3a52909f98454e5caee1a4
Kzsanjeet <sanjeetkazithapa@gmail.com>
2024-06-06 23:11:35 +0545

    learned about the use state and added uppercase conversion features

--- react/my-app/src/components/TextForm.js
+++ react/my-app/src/components/TextForm.js
@@ -1,18 +1,31 @@
-import React, {useState} from 'react'
-
-
+import React, { useState } from 'react';
 
 export default function TextForm(props) {
-    const[text, setText] = useState("Enter text here")
-    // text = "new text" // wrong way to change the state 
-    // setText("new text") // correct way to change the state
+    const handleUpClick = () => {
+        // console.log("Uppercase was clicked" + text);
+        setText(text.toUpperCase());
+    }
+
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState("Enter text here");
+
     return (
         <div>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" id="myBox" value={text} rows="3"></textarea>
+                <textarea 
+                    className="form-control" 
+                    id="myBox" 
+                    value={text} 
+                    onChange={handleOnChange} 
+                    rows="3">
+                </textarea>
             </div>
-            <button className="btn btn-primary">Convert to Uppercase</button>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
         </div>
     )
 }
