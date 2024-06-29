8ad954e9beb489bf7b9140bd97c58b21786c9796
Kzsanjeet <sanjeetkazithapa@gmail.com>
2024-06-29 23:21:44 +0545

    learned about the jsx rules

--- fullReact/tutorial/src/index.js
+++ fullReact/tutorial/src/index.js
@@ -2,13 +2,24 @@ import React from "react"
 import ReactDom from "react-dom"
 
 function Greeting(){
-  return <h4>thid is sanjeet thapa this is my first component</h4>
+  return (
+    <div>
+        <h4>thid is sanjeet thapa this is my first component</h4>
+        <div>
+        <ul>
+          <li>
+            <a href="index.html">Hello World</a>
+          </li>
+        </ul>
+        </div>  
+    </div>
+)
 } 
 
 ReactDom.render(<Greeting/>,document.getElementById("root"))
 
 
 // component must always be capitalize
-
+// you must return sth after initializing component
 
 
