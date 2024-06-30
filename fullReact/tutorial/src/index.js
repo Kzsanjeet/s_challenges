5cafca0f82e51433e83676d33f571d9ce3882450
Kzsanjeet <sanjeetkazithapa@gmail.com>
2024-06-30 23:16:10 +0545

    learned about the component in react

--- fullReact/tutorial/src/index.js
+++ fullReact/tutorial/src/index.js
@@ -1,25 +1,51 @@
-import React from "react"
-import ReactDom from "react-dom"
+// import React from "react"
+// import ReactDom from "react-dom"
 
-function Greeting(){
-  return (
-    <div>
-        <h4>thid is sanjeet thapa this is my first component</h4>
-        <div>
-        <ul>
-          <li>
-            <a href="index.html">Hello World</a>
-          </li>
-        </ul>
-        </div>  
-    </div>
-)
-} 
 
-ReactDom.render(<Greeting/>,document.getElementById("root"))
+
+// function Greeting(){
+//   return (
+//     <div>
+//         <h4>thid is sanjeet thapa this is my first component</h4>
+//         <div>
+//         <ul>
+//           <li>
+//             <a href="index.html">Hello World</a>
+//           </li>
+//         </ul>
+//         </div>  
+//     </div>
+// )
+// } 
+
+
+
+// ReactDom.render(<Greeting/>,document.getElementById("root"))
 
 
 // component must always be capitalize
 // you must return sth after initializing component
+
+
+
+import React from "react"
+import ReactDom from "react-dom"
+
+//nested components, react tools
+
+function Greeting(){
+  return (
+    <div>
+       <Person/>
+       <Person2/> 
+    </div>
+)
+} 
+
+const Person = () => <h2>Saanjeet Thapa</h2>
+const Person2 = () => <h2>Sanjeet Thapa</h2>
+
+ReactDom.render(<Greeting/>,document.getElementById("root"))
+
 
 
