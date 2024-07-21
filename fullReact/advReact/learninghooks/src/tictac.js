ef5e6839ccb3e346521c843d1666da9e22ea7323
Kzsanjeet <sanjeetkazithapa@gmail.com>
2024-07-21 19:03:59 +0545

    made simple game tic tac toe my first project of learning react"

--- fullReact/advReact/learninghooks/src/tictac.js
+++ fullReact/advReact/learninghooks/src/tictac.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 
 //making sqaure component to show X when the button is clicked
-function Square({value}){
+function Square({value, onSquareClick}){
     // return <button className="square">{value}</button>
     // const [value, setValue] = useState(null)
     // function handleClick(){
@@ -20,34 +20,76 @@ function Square({value}){
 }
 
 
-export default function Board(){
-    const [squares, setSquares] = useState(Array(9).fill(null))   //Array(9).fill(null) creates an array with nine elements and sets each of them to null.
+export default function Board() {
+    const [xIsNext,setXIsNext] = useState(true)
+    const [squares, setSquares] = useState(Array(9).fill(null));
+  
+    function handleClick(i) {
+        if(squares[i] || calculateWinner(squares)){
+            return  // prevents overwrite   this directly returns the value of original array
+        }
 
+      const nextSquares = squares.slice();
+      if(xIsNext){
+        nextSquares[i] = "X"
+      }else{
+        nextSquares[i] = "O"
+      }
+      setSquares(nextSquares)
+      setXIsNext(!xIsNext)  //This function call updates the state with the inverted value of xIsNext, effectively toggling the turn from "X" to "O" or from "O" to "X".
+    }
 
-    function handleClick(i){
-        const nextSquares = squares.slice();
-        nextSquares[i] = "X";
-        setSquares(nextSquares);
+    const winner = calculateWinner(squares)
+    let status;
+    if(winner){
+        status = "Winner: " + winner
+        }else{
+        status = "Next player: " + (xIsNext ? "X" : "O")
+         }
 
+    const reload = () =>{
+        window.location.reload()
     }
     return (
-    <>
-    <div className="board-row">
-        <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
-        <Square value={squares[1]}/>
-        <Square value={squares[2]}/>
-    </div>
-    <div className="board-row">
-        <Square value={squares[3]}/>
-        <Square value={squares[4]}/>
-        <Square value={squares[5]}/>
-    </div>
-    <div className="board-row">
-        <Square value={squares[6]}/>
-        <Square value={squares[7]}/>
-        <Square value={squares[8]}/>
-    </div>
-    </>    
-    
-    )
-}
+      <>
+      <div><button onClick={reload}>Refresh</button></div>
+      <div className="status">{status}</div>
+        <div className="board-row">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+        </div>
+        <div className="board-row">
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+        </div>
+        <div className="board-row">
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+        </div>
+      </>
+    );
+  }
+
+  function calculateWinner(squares){
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+        ]
+
+        for (let i = 0; i<lines.length; i++){
+            const [a, b, c] = lines[i]
+            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){  // Ensures that the square at index a is not null or undefined (i.e., it has been filled with either "X" or "O").
+                return squares[a]
+            }
+        }
+        return null
+  }
